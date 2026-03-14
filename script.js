f295cf2f4d22a03adf6c145ad64492bbcc730eee
Ellen Dee Dego <189423563+ecdego7@users.noreply.github.com>
2026-03-15 07:40:59 +0800

    Initial commit - mini-project web profile

---
+++ script.js
@@ -0,0 +1,32 @@
+const funfacts = [
+    "I played in a marching band all my high school years. I played the baritone",
+    "I started to be interested in coding at the start of 2023.",
+    "One of my produest moments was taking the public bus to go on an hour-long trip to Hiroshima City all by myself.",
+    "I work around footfall traffic on some North American real estate properties.",
+];
+
+const button = document.getElementById("funfactbutton");
+const output = document.getElementById("funfacttext");
+
+
+let timer;
+let clickedonce = false;
+
+
+if (button && output) {
+    button.addEventListener("click", () => {
+        const randomIndex = Math.floor(Math.random() * funfacts.length);
+        output.textContent = funfacts[randomIndex];
+
+        if (!clickedonce) {
+            button.textContent = "Show Another Fun Fact";
+            clickedonce = true;
+        }
+
+        clearTimeout(timer);
+
+        timer = setTimeout(function () {
+            output.textContent = "";
+        }, 4000);
+    });
+}
