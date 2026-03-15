e4df8b49a07fa3f8b96bb41f39d5cb1eafb7ee2a
Ellen Dee Dego <189423563+ecdego7@users.noreply.github.com>
2026-03-15 12:40:18 +0800

    Minor changes for mobile layout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const funfacts = [
     "I played in a marching band all my high school years. I played the baritone",
     "I started to be interested in coding at the start of 2023.",
-    "One of my produest moments was taking the public bus to go on an hour-long trip to Hiroshima City all by myself.",
+    "One of my proudest moments was taking the public bus to go on an hour-long trip to Hiroshima City all by myself.",
     "I work around footfall traffic on some North American real estate properties.",
 ];
 
